db2bab452936e2735890bf36e9a06ba3c3457d20
FrancieleCastilho <francielecasteves@gmail.com>
2018-10-03 21:47:38 -0300

    Formulario

--- ambiente/aula02/index.php
+++ ambiente/aula02/index.php
@@ -26,6 +26,14 @@
         <body>
         <h1>Tickets</h1>
         <p>Gerenciador de tickets</p>
+
+        <!-- formulario de cadastro -->
+    <form action="index.php" method="post">
+            <label>Problema</label>
+            <input type="text" name="titulo"/>
+            <input type="submit"name="botao_adicionar" value="Adicionar"/>
+    </form>
+
         <?php
             $servidor = "ambiente_db_1";
             $usuario = "root";
@@ -37,28 +45,57 @@
             if($con->connect_error){
                 die("Falha de conexão: " . $conn->connect_error);
             }
+            // Tratar dados enviados para a pagina
+
+            if($_POST["titulo"]!= ""){
+                $sql = "INSERT INTO `tickets` (`titulo`,`status`) VALUES 
+                ('".$_POST["titulo"]."', 0)";
+                if($conn->query($sql)===TRUE){
+                    echo "Ticket Adicionado!";
+                }else{
+                    echo "Ocorreu um erro:".sql."<br/>".$conn->error;
+                }
+            }
+
+            // recuperar todos os registros da tabela tickets
+            $sql = "SELECT * FROM tickets";
+            $result = $conn->query($sql);
         ?>
-            <table>
-            <tr>
-                <th>ID</th>
-                <th>Título</th>
-                <th>Status</th>
-            </tr>
-                <tr>
-                    <td>1</td> 
-                    <td>Criar novo Usuario</td> 
-                    <td>Aberto</td> 
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Analisar fatura</td>
-                    <td>Aberto</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Criar novo usuario</td>
-                    <td>Aberto</td>
-                </tr>
-            </table>
-        </body>
+            
+
+                <?php
+                    if($result->num_rows > 0){
+                ?>
+                        <table>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Título</th>
+                                        <th>Status</th>
+                                    </tr>
+                        
+                        <?php
+                            while($row = $result->fetch_assoc()){
+                                echo "<tr>";
+                                echo "<td>". $row["id"]."</td>";
+                                echo "<td>". $row["titulo"]."</td>";
+                                echo "<td>";
+                                if($row["status"]==0){
+                                    echo "Aberto";
+                                }else{
+                                    echo"Fechado";
+                                }
+                                
+                                echo "</td>";
+                                echo "</tr>";
+                            }
+                            ?>
+                        </table>
+                        <?php
+                    }else{
+                        echo "Nenhum registro";
+                    }
+
+                ?>
+
+              </body>
 </html>
