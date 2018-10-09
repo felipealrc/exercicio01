efba643ff2a4ecbf2496a38e9a8873c7b04e8d02
Franciele Castilho <francielecasteves@gmail.com>
2018-10-08 21:08:37 -0300

    Merge da aula 3

---
+++ aula03/usuario.php
@@ -0,0 +1,17 @@
+<?php
+
+class usuario {
+    public $id;
+    public $nome;
+    public $email;
+    public $senha;
+
+}
+$usuario = new usuario();
+
+$usuario->id = "1";
+$usuario->nome = "Judas Silva";
+$usuario->email = "judassilva@gmail.com";
+$usuario->senha = "1234";
+ 
+echo  
